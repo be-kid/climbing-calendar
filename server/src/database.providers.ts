4b4ba80cd6d5d431c4a03a6c65aac8e9fb4f62e1
LeeJooYong <daramzycoder@gmail.com>
2022-06-14 23:55:46 +0900

    환경 변수 설정부터 다시 해야함

--- server/src/database.providers.ts
+++ server/src/database.providers.ts
@@ -1,7 +1,7 @@
 import * as mongoose from 'mongoose';
 
 const uri =
-  'mongodb+srv://admin:Py22qYJorLYH3Kmu@climbing.zahev.mongodb.net/?retryWrites=true&w=majority';
+  `mongodb+srv://admin:${process.env.DB_SECRET}@climbing.zahev.mongodb.net/?retryWrites=true&w=majority`;
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
