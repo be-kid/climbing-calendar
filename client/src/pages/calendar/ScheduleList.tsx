cffd0eb37ae35fb317302b4719332e01a752e27c
LeeJooYong <daramzycoder@gmail.com>
2022-06-19 17:13:18 +0900

    요일 안맞는거 수정

--- client/src/pages/calendar/ScheduleList.tsx
+++ client/src/pages/calendar/ScheduleList.tsx
@@ -3,11 +3,6 @@ import Day from "./component/Day";
 
 export default function ScheduleList(props: any) {
   const { curCalendar, year, month } = props;
-  // TODO
-  // year, month로 date 생성
-  // 요일은 0-일요일 부터 6-토요일까지
-  // date 만큼 Day 생성
-  // Day에는 해당 날짜의 스케쥴을 넘겨줌
 
   const lastDate = new Date(year, month, 0).getDate();
   const thisMonth = new Array(lastDate).fill([]);
@@ -20,7 +15,7 @@ export default function ScheduleList(props: any) {
   return (
     <Stack sx={{ width: 400, textAlign: "center" }}>
       {thisMonth.map((day: any, idx: number) => {
-        const today = new Date(year, month, idx + 1).getDay();
+        const today = (new Date(year, month + 1, idx + 1).getDay() + 1) % 7;
         return (
           <Day key={idx} date={idx + 1} day={today} participants={day}></Day>
         );
