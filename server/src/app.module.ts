1ff958ea4d1073d6ae3d97f3a55ff4efa6b3433a
LeeJooYong <daramzycoder@gmail.com>
2022-06-22 15:57:49 +0900

    배포용 url로 번경

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -11,7 +11,7 @@ import { ScheduleModule } from "./models/schedule.module";
       typePaths: ["./**/*.graphql"],
       driver: ApolloDriver,
       cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
         credentials: true,
       },
     }),
